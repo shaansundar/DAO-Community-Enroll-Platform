203f82c85a57d251d0f7933bdcef364befa465ba
shaansundar <shaan.idtindia@gmail.com>
2022-05-18 23:34:36 +0530

    v1.0.5 - Timestamp fixed

--- src/app/enrolled-list/enrolled-list.component.ts
+++ src/app/enrolled-list/enrolled-list.component.ts
@@ -30,4 +30,17 @@ export class EnrolledListComponent implements OnInit {
     console.log(this.data);
     this.isLoading = false;
   }
+
+  public timeConverter(UNIX_timestamp: any){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = "0"+ a.getMinutes();
+    var sec = "0"+ a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' - ' + hour + ':' + min.substr(-2) + ':' + sec.substr(-2) ;
+    return time;
+  }
 }
