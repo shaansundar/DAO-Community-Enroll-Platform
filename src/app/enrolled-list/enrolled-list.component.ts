dcf9d4bbd60a48a60743a980a998a8f093762f0b
shaansundar <shaan.idtindia@gmail.com>
2022-05-23 17:35:51 +0530

    v1.1.0 - Starting Upgradable extraction of db

--- src/app/enrolled-list/enrolled-list.component.ts
+++ src/app/enrolled-list/enrolled-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ethers } from 'ethers';
 import contractAddress from '../../../env/contractAddress.json';
 import DAO from '../../../blockchain/artifacts/blockchain/contracts/Database.sol/Database.json';
+const fs = require('fs')
 @Component({
   selector: 'app-enrolled-list',
   templateUrl: './enrolled-list.component.html',
@@ -11,6 +12,7 @@ export class EnrolledListComponent implements OnInit {
   public provider: any = new ethers.providers.JsonRpcProvider('https://rpc.testnet.fantom.network');
   public DAOInterface: any;
   public data: any;
+  public emails: any;
   public isLoading: boolean = true;
   constructor() {
     this.DAOInterface = new ethers.Contract(
@@ -20,8 +22,9 @@ export class EnrolledListComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {
-    this.getData();
+  async ngOnInit(): Promise<void> {
+    await this.getData();
+    this.download();
   }
 
   public async getData() {
@@ -31,16 +34,27 @@ export class EnrolledListComponent implements OnInit {
     this.isLoading = false;
   }
 
+  public download(){
+    this.emails = this.extractEmail();
+
+  }
+
+  public extractEmail(){
+    let emails = this.data.map((e:any)=>(e[5].replaceAll()))
+    const data = JSON.stringify(emails);
+    return data;
+  }
+
   public timeConverter(UNIX_timestamp: any){
     var a = new Date(UNIX_timestamp * 1000);
     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
-    var hour = a.getHours();
+    var hour = "0"+a.getHours();
     var min = "0"+ a.getMinutes();
     var sec = "0"+ a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' - ' + hour + ':' + min.substr(-2) + ':' + sec.substr(-2) ;
+    var time = date + ' ' + month + ' ' + year + ' - ' + hour.substr(-2) + ':' + min.substr(-2) + ':' + sec.substr(-2) ;
     return time;
   }
 }
